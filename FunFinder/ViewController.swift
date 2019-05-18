848b871004624c4988c28cd531a01a2932d8e51b
Corey <Corey@Coreys-MacBook-Air.local>
2019-05-18 13:55:52 -0600

    Initial Commit

---
+++ FunFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFinder
+//
+//  Created by Corey  on 5/18/19.
+//  Copyright © 2019 Corey . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
