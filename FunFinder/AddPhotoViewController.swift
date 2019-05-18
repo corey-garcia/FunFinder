2d7c9b3f0496b9ca365caf42c8ff163e445b94a8
Corey <Corey@Coreys-MacBook-Air.local>
2019-05-18 15:49:42 -0600

    Iteration One: Completed

---
+++ FunFinder/AddPhotoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AddPhotoViewController.swift
+//  FunFinder
+//
+//  Created by Corey  on 5/18/19.
+//  Copyright © 2019 Corey . All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //want to update the image on screen with selected image
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        
+        //then call the storyboard with the updated image to the top of the stack
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
